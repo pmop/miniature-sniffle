6878cb921fd2dd4c60fbe9c266535825353d4d05
Pedro Pinheiro <pedro@tidy.com>
2022-05-20 04:13:26 -0400

    Remove form and do small refactor

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -3,6 +3,9 @@ import consumer from "channels/consumer"
 
 export default class extends Controller {
   connect() {
+    this.data.startDate = '';
+    this.data.endDate = '';
+
     consumer.subscriptions.create(
       { channel: "DateRangeChannel" , request_id: this.data.get('request') },
       {
@@ -16,11 +19,7 @@ export default class extends Controller {
     this.fetchDateRangesAndRender()
   }
 
-  renderDatePicker(blocked_date_ranges) {
-    const toDateRange = (e) => [e.start_date, e.end_date]
-    const blockedDateRanges = Array.from(blocked_date_ranges.map(toDateRange));
-    console.log('blockedDateRanges', blockedDateRanges)
-
+  renderDatePicker() {
     const picker = new Litepicker({
       element:                 document.getElementById('datepicker'),
       scrollToDate:            new Date(),
@@ -29,11 +28,10 @@ export default class extends Controller {
       position:                'bottom',
       footer:                  true,
       disallowLockDaysInRange: true,
-      lockDays:                blockedDateRanges,
+      lockDays:                this.data.blockedDateRanges,
       setup:                   (picker) => {
         picker.on('selected', (date1, date2) => {
           this.setDatesToSend();
-
         });
       }
     });
@@ -42,20 +40,35 @@ export default class extends Controller {
   }
 
   setDatesToSend() {
-    const startDate = this.picker.getStartDate().toJSDate().toDateString();
-    const endDate = this.picker.getEndDate().toJSDate().toDateString();
+    this.data.startDate = this.picker.getStartDate().toJSDate().toDateString();
+    this.data.endDate = this.picker.getEndDate().toJSDate().toDateString();
     console.log(
       this.picker.getStartDate().toJSDate(),
       this.picker.getEndDate().toJSDate()
     );
-
-    document.getElementById('startDate').setAttribute('value', startDate);
-    document.getElementById('endDate').setAttribute('value', endDate);
   }
 
   fetchDateRangesAndRender() {
+    const toDateRange = (e) => [e.start_date, e.end_date]
+
     fetch('/calendar', { headers: { 'Accept': 'application/json' } } )
       .then(response => response.json())
-      .then(data => this.renderDatePicker(data) )
+      .then(data => this.setBlockedRanges(Array.from(data.map(toDateRange))))
+      .then(data => this.renderDatePicker())
+  }
+
+  setBlockedRanges(blockedRanges) {
+    console.log('blockedRanges', blockedRanges)
+    this.data.blockedDateRanges = blockedRanges;
+  }
+
+  createDateRangeBlock() {
+    if((this.data.startDate.length > 0) && (this.data.endDate.length > 0)) {
+      // do post request
+
+    }
+    else {
+      console.log('undefined dates');
+    }
   }
 }
